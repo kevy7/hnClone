9f90af55a368dbb9c27a2efe16bba0abd28cbec0
kevy7 <kvnyang503@gmail.com>
2019-08-02 00:28:45 -0700

    Updates

---
+++ precache-manifest.912975487a843dff2652ccf4078e631f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc47246cd03f61f47a66",
+    "url": "/hnClone/static/css/main.099898bb.chunk.css"
+  },
+  {
+    "revision": "bc47246cd03f61f47a66",
+    "url": "/hnClone/static/js/main.bc47246c.chunk.js"
+  },
+  {
+    "revision": "580b24aab52955cde662",
+    "url": "/hnClone/static/js/1.580b24aa.chunk.js"
+  },
+  {
+    "revision": "fb59f690fb0f99006cb5",
+    "url": "/hnClone/static/js/runtime~main.fb59f690.js"
+  },
+  {
+    "revision": "7b45604a0799b28ffd6aba210e34ff9d",
+    "url": "/hnClone/index.html"
+  }
+];
